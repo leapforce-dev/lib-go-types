9dc46bb6cca5d43820da74342de89200df53e2ac
caselongo <caselongo@gmail.com>
2021-03-11 14:25:31 +0100

    x

---
+++ Int64String.go
@@ -0,0 +1,37 @@
+package types
+
+import (
+	"strconv"
+	"strings"
+)
+
+type Int64String int64
+
+func (i *Int64String) UnmarshalJSON(b []byte) error {
+	unquoted, err := strconv.Unquote(string(b))
+	if err != nil {
+		return nil
+	}
+
+	if strings.Trim(unquoted, " ") == "" {
+		i = nil
+		return nil
+	}
+
+	_i, err := strconv.ParseInt(unquoted, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	*i = Int64String(_i)
+	return nil
+}
+
+func (i *Int64String) Value() *int64 {
+	if i == nil {
+		return nil
+	}
+
+	_i := int64(*i)
+	return &_i
+}
